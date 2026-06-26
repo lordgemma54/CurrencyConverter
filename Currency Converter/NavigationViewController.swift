53189ebb69b711f085f9c58d3b70908817dacfda
RTV <rtvconcepts@gmail.com>
2026-06-26 16:19:11 -0400

    initial commit

---
+++ Currency Converter/NavigationViewController.swift
@@ -0,0 +1,26 @@
+//
+//  NavigationViewController.swift
+//  Currency Converter
+//
+//  Created by rahul v on 6/26/26.
+//
+
+import UIKit
+
+class NavigationViewController: UIViewController {
+
+    var currency1 = ""
+
+    @IBOutlet weak var curLabel_1: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        curLabel_1.text = "currency coming soon"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+
+}
--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    	
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func convertButtonAction(_ sender: UIButton) {
+        self.performSegue(withIdentifier: "toNavigation", sender: self)
+    }
+    
+    
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+        if(segue.identifier == "toNavigation") {
+            let navigation = segue.destination as! NavigationViewController
+            navigation.currency1 = "hello everyone"
+        }
+    }
+    
 }
 
