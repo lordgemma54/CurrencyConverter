2386d5dd9cdb3ba3c496c4c0ca09d448b1d7bc81
RTV <rtvconcepts@gmail.com>
2026-06-26 17:56:51 -0400

    stupid constraints in place for the 9th time. User input validated

--- Currency Converter/ViewController.swift
+++ Currency Converter/ViewController.swift
@@ -9,16 +9,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    	
+    @IBOutlet weak var dollarEntry: UITextField!
+    var roundDollars : Int = 0
+    
+    @IBOutlet weak var errorLabel: UILabel!
+    
+    var switch_1_on = true
+    var switch_2_on = true
+    var switch_3_on = true
+    var switch_4_on = true
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
-    @IBAction func convertButtonAction(_ sender: UIButton) {
-        self.performSegue(withIdentifier: "toNavigation", sender: self)
-    }
     
     
     // MARK: - Navigation
@@ -31,6 +39,71 @@ class ViewController: UIViewController {
             let navigation = segue.destination as! NavigationViewController
             navigation.currency1 = "hello everyone"
         }
+    }
+    
+    @IBAction func curSwitch_1(_ sender: UISwitch) {
+        if sender.isOn {
+            switch_1_on = true
+        } else {
+            switch_1_on = false
+        }
+    }
+    
+    @IBAction func curSwitch_2(_ sender: UISwitch) {
+        if sender.isOn {
+            switch_2_on = true
+        } else {
+            switch_2_on = false
+        }
+    }
+    
+    @IBAction func curSwitch_3(_ sender: UISwitch) {
+        if sender.isOn {
+            switch_3_on = true
+        } else {
+            switch_3_on = false
+        }
+    }
+    
+    @IBAction func curSwitch_4(_ sender: UISwitch) {
+        if sender.isOn {
+            switch_4_on = true
+        } else {
+            switch_4_on = false
+        }
+    }
+    
+
+    @IBAction func convertBtn(_ sender: UIButton) {
+        errorLabel.isHidden = true
+        
+        guard let userInput = dollarEntry.text, !userInput.isEmpty else {
+            errorLabel.text = "Please enter a dollar amount"
+            errorLabel.isHidden = false
+            return
+        }
+        
+    
+        if let integerInput = Int(userInput) {
+            roundDollars = integerInput
+            
+        }else if let floatInput = Double(userInput) {
+            let roundedDouble = floatInput.rounded()
+            roundDollars = Int (roundedDouble)
+            errorLabel.text = "Input rounded"
+            errorLabel.isHidden = false
+        } else {
+            errorLabel.text = ("Please enter a valid dollar amount")
+            errorLabel.isHidden = false
+            return
+        }
+        
+        dollarEntry.text = String(roundDollars)
+        dollarEntry.resignFirstResponder()
+        
+        self.performSegue(withIdentifier: "toNavigation", sender: self)
+        
+        
     }
     
 }
